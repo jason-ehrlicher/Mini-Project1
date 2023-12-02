4498e451cb462443c257de3a9efca49245479e76
Jason Ehrlicher <jehrlich@uno.edu>
2023-12-01 21:19:50 -0600

    created and linked html pages

---
+++ js/charts.js
@@ -0,0 +1,156 @@
+// Charts
+
+// Bar Chart
+
+var barChartOptions = {
+    series: [
+      {
+        data: [138, 70, 60, 120, 70, 140, 127],
+      },
+    ],
+    chart: {
+      type: "bar",
+      height: 350,
+      toolbar: {
+        show: false,
+      },
+    },
+    colors: [
+      "#184e77",
+      "#1e6091",
+      "#1a759f",
+      "#168aad",
+      "#34a0a4",
+      "#52b69a",
+      "#76c893",
+    ],
+  
+    plotOptions: {
+      bar: {
+        distributed: true,
+        borderRadius: 4,
+        horizontal: false,
+        columnWidth: "40%",
+      },
+    },
+    dataLabels: {
+      enabled: false,
+    },
+    legend: { show: false },
+    xaxis: {
+      categories: [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+      ],
+    },
+    yaxis: {
+      title: {
+        text: "Rounds Played",
+      },
+    },
+  };
+  
+  var barChart = new ApexCharts(
+    document.querySelector("#bar-chart"),
+    barChartOptions
+  );
+  barChart.render();
+  
+  // Area Chart
+  var options = {
+    series: [
+      {
+        name: "Rounds Per Month",
+        data: [
+          2253, 2275, 3496, 3659, 3704, 2960, 3084, 2690, 3088, 3439, 2158, 1935,
+        ],
+      },
+    ],
+    chart: {
+      height: 350,
+      type: "line",
+      toolbar: {
+        show: false,
+      },
+      zoom: {
+        enabled: false,
+      },
+    },
+    dataLabels: {
+      enabled: false,
+    },
+    stroke: {
+      width: [5, 7, 5],
+      curve: "straight",
+      dashArray: [0, 8, 5],
+    },
+    legend: {
+      tooltipHoverFormatter: function (val, opts) {
+        return (
+          val +
+          " - " +
+          opts.w.globals.series[opts.seriesIndex][opts.dataPointIndex] +
+          ""
+        );
+      },
+    },
+    markers: {
+      size: 0,
+      hover: {
+        sizeOffset: 6,
+      },
+    },
+    xaxis: {
+      categories: [
+        "Jan",
+        "Feb",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "Aug",
+        "Sept",
+        "Oct",
+        "Nov",
+        "Dec",
+      ],
+    },
+    tooltip: {
+      y: [
+        {
+          title: {
+            formatter: function (val) {
+              return val + " (mins)";
+            },
+          },
+        },
+        {
+          title: {
+            formatter: function (val) {
+              return val + " per session";
+            },
+          },
+        },
+        {
+          title: {
+            formatter: function (val) {
+              return val;
+            },
+          },
+        },
+      ],
+    },
+    grid: {
+      borderColor: "#f1f1f1",
+    },
+  };
+  
+  var areaChart = new ApexCharts(document.querySelector("#area-chart"), options);
+  areaChart.render();
+  
