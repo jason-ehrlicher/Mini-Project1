c1ede689ca334a73aa0dd68f146633915d8f3353
Jason Ehrlicher <jehrlich@uno.edu>
2023-12-01 19:45:12 -0600

    code formatting

--- js/landing.js
+++ js/landing.js
@@ -1,14 +1,14 @@
-
 // Modal
 
-// Modal Open 
-document.getElementById("signup-link").addEventListener('click', function(event) {
-  event.preventDefault();
-document.querySelector(".login-modal").style.display = 'flex';
-})
+// Modal Open
+document
+  .getElementById("signup-link")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    document.querySelector(".login-modal").style.display = "flex";
+  });
 
 // Modal Close
-document.querySelector('.modal-close').addEventListener('click',
-function() {
-  document.querySelector('.login-modal').style.display = 'none'
+document.querySelector(".modal-close").addEventListener("click", function () {
+  document.querySelector(".login-modal").style.display = "none";
 });
