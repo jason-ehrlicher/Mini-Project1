bb8f668a5ede72dcbac9830ad8ef9c79048fe289
Jason Ehrlicher <jehrlich@uno.edu>
2023-12-01 18:52:23 -0600

    Created Sign Up Modal

--- js/landing.js
+++ js/landing.js
@@ -3,6 +3,7 @@
 let sidebarOpen = false;
 let sidebar = document.getElementById("sidebar");
 
+// Sidebar Open
 function openSidebar() {
   if (!sidebarOpen) {
     sidebar.classList.add("sidebar-responsive");
@@ -10,9 +11,24 @@ function openSidebar() {
   }
 }
 
+// Sidebar Close
 function closeSidebar() {
   if (sidebarOpen) {
     sidebar.classList.remove("sidebar-responsive");
     sidebarOpen = false;
   }
 }
+
+// Modal
+
+// Modal Open 
+document.getElementById("signup-link").addEventListener('click', function(event) {
+  event.preventDefault();
+document.querySelector(".login-modal").style.display = 'flex';
+})
+
+// Modal Close
+document.querySelector('.modal-close').addEventListener('click',
+function() {
+  document.querySelector('.login-modal').style.display = 'none'
+});
